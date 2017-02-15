825ea9e96afaeaad5a975b7c28b1767398f01388
Vagner Barbosa <contato@vagnerbarbosa.com>
2017-02-15 17:20:52 -0300

    :ok:

--- src/main/java/riodopeixe/rest/resource/ApplicationConfig.java
+++ src/main/java/riodopeixe/rest/resource/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package riodopeixe.rest.resource;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 
 /**
  *
@@ -9,6 +12,34 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("/webservice")
 public class ApplicationConfig extends Application {
+
+    private final Set<Object> singletons = new HashSet<Object>();
+    private final HashSet<Class<?>> classes = new HashSet<Class<?>>();  
+
+    public ApplicationConfig() {
+        CorsFilter corsFilter = new CorsFilter();
+        corsFilter.getAllowedOrigins().add("*");
+        corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
+        singletons.add(corsFilter);
+
+        classes.add(HelloWorldResource.class);
+        classes.add(InvoiceResource.class);        
+        classes.add(SalesOrderResource.class);
+        classes.add(SalesResource.class);
+        classes.add(SupplierResource.class);
+        classes.add(SysInvoiceResource.class);
+        classes.add(TonerResource.class);
+    }
+    
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+
+    @Override
+    public HashSet<Class<?>> getClasses(){
+      return classes;
+    }    
 
     
 }
